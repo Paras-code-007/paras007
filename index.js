ca439793dc2bc865be718be1c4d3bce8d1abc001
Paras Arora <parasarora@Parass-MacBook-Air.local>
2020-10-25 14:19:03 +0530

    First Version

---
+++ index.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+console.log(`
+Paras Arora - WEB DEVELOPER | AR VR Enthusiast
+
+An aspiring enthusiast 
+A key learner, want to gather as much information as i can
+Some key Projects:
+Shopvela: http://shopvela.herokuapp.com/
+
+Profile links
+Github: https://github.com/Paras-code-007
+Twitter: https://twitter.com/paras0025
+linkedin: https://www.linkedin.com/in/paras-arora-343470197/
+`)
+
