df735d470b909ce4f7170b7f2fc027486616a915
Paras Arora <parasarora@Parass-MacBook-Air.local>
2020-10-26 10:35:10 +0530

    Improved: now clear console too: minor release

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 #!/usr/bin/env node
 
+// console.clear()
+// console.log(process)
+// console.log(process.stdout)
+// process.stdout.write(process.platform === 'win32' ? '\x1B[2J\x1B[0f' : '\x1B[2J\x1B[3J\x1B[H')
+// if we isntall pkg for the same
+
+const clearconsole= require('clear-any-console')
+clearconsole()
+
 console.log(`
 Paras Arora - WEB DEVELOPER | AR VR Enthusiast
-
 An aspiring enthusiast 
 A key learner, want to gather as much information as i can
 Some key Projects:
