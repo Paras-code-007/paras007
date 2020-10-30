07b9ebdc1061cef3e9c791b8751ae61758d583f0
Paras Arora <parasarora@Parass-MacBook-Air.local>
2020-10-30 15:20:43 +0530

    removed unwanted comments and uncommented debug data

--- index.js
+++ index.js
@@ -15,23 +15,9 @@ const args= process.argv.slice(2) //to get only the flags and inputs and alias t
     if(cli.flags.social) console.log(socialHandles)        
     if(cli.flags.fact) alert({type: 'info', msg: fact, name: 'fun fact'})
 
-    // alert({type: 'info', msg: 'here is cli⬇' })
+    alert({type: 'info', msg: 'here is cli debug data⬇' })
     // console.log('cli', cli)
-    // console.log('cli.flags',cli.flags)
-    // console.log('cli.input',cli.input)
-    // alert({type: 'success', msg: 'showVersion fn'})
-    // console.log(cli.showVersion()) //includes process.exit and show version formpkgjson file and is also executed when provided with a --version flag
-    // alert({type: 'success', msg: 'showHelp fn'})
-    // console.log(cli.showHelp())  //includes process.exit and show helptext and is also executed when provided with a --help flag
-    
+    console.log('cli.flags',cli.flags)
+    console.log('cli.input',cli.input)
+        
 })()
-
-// --flag1-hel is displayed as flag1Hel (camelcase)
-// Parass-MacBook-Air:paras007 parasarora$ paras007 --flag1 --flag2 --no-social
-//   flags: { flag1: true, flag2: true, social: false, fact: true },
-//if default not mention in meow then the flag will be false
-// Parass-MacBook-Air:paras007 parasarora$ paras007 --flag1 --flag2 -s-f
-// flags: { flag1: true, flag2: true, social: false, fact: true },
-
-// Parass-MacBook-Air:paras007 parasarora$ paras007 --flag1 --flag2 -sf  //will just include one more flag that is f
-//   flags: { flag1: true, flag2: true, social: true, f: true, fact: true },
