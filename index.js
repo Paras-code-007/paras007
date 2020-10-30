1311e3fe74083aea69de91c951c165b5d52b5f20
Paras Arora <parasarora@Parass-MacBook-Air.local>
2020-10-30 15:47:57 +0530

    added:  paras007 help using showHelp fn

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ const {bio,socialHandles,projects,fact}= require('./utils/data.js')
 const cli= require('./utils/cli')
 const debug= require('./utils/debug.js')
 const alert= require('clialerting')
+const { input } = require('./utils/cli')
 
 const args= process.argv.slice(2) //to get only the flags and inputs and alias to flags
 
 ;(async ()=>{
 
     init()
+    cli.input.includes('help') && cli.showHelp(0)  //by default exit code of show help is 2 but say rightly executed thefore changed exitcode to 0
+    // you can use echo $? to check previous command exitcode
     if(cli.flags.bio) console.log(bio)
     if(cli.flags.projects) console.log(projects)
     if(cli.flags.social) console.log(socialHandles)        
