a62eacef9f9e1b983f3285bcd5e6f9d952785523
Paras Arora <parasarora@Parass-MacBook-Air.local>
2020-10-31 06:37:46 +0530

    spinner sample1

--- utils/stats.js
+++ utils/stats.js
@@ -1,15 +1,17 @@
+const ora= require('ora')
 const handleError= require('cli-handle-error')
 const to = require('await-to-js').default;
-const fetch= require('node-fetch')
 const axios= require('axios')
 const apiUrl= 'https://api.github.com/users/Paras-code-007'
 
 module.exports= async ()=>{
+    const spinner = ora({text: ''})
+    spinner.start('fetching github followers')
     const [err,res]= await to(axios.get(apiUrl)   )
     handleError('API CALL FAILED',err,true,true)  //exit rightaway when got error / no line after this execute
     // console.log(res)
     const ghfollower= res.data.followers
-
+    spinner.stop()
     console.log(`
 Github Followers: ${ghfollower}
     `)
