05bf0c43d3fcb1635445946a2874b55013ae2488
Paras Arora <parasarora@Parass-MacBook-Air.local>
2020-10-31 05:32:14 +0530

    use axios

--- utils/stats.js
+++ utils/stats.js
@@ -1,64 +1,18 @@
-const fetch= require('node-fetch')
+// const fetch= require('node-fetch')
 const axios= require('axios')
 const apiUrl= 'https://api.github.com/users/Paras-code-007'
 
-//Promise implementation
-//+using fetch
-/*
-module.exports= ()=>{
-    fetch(apiUrl).then((res)=>{
-        // console.log(res)
-        // console.log(res.json())  //Promise { <pending> }
-        if(res.ok){
-            console.log('success')
-            return res.json()  
-        }else{
-            const err= new Error('unhandled error: 404 response')
-            console.log('err',err)
-        }
-        
-    }) //Promise { <pending> }
-    .then((data)=>{
-        // console.log(data)
-        console.log(data.followers)
-    })
-    .catch((err)=>{
-        console.log(err)
-    })
-}
-*/
-
-//+using axios
-/*
-module.exports= ()=>{
-    // console.log(axios.get(apiUrl)) //Promise { <pending> }
-    axios.get(apiUrl)
-    .then((res)=>{
-        // console.log(res)
-        // console.log(res.data)
-        console.log(res.data.followers)
-    })
-    .catch((err)=>{
-        console.log(err.response.status)
-    })
-}
-*/
-
-//async await 
-/*
 module.exports= async ()=>{
-    const res= await fetch(apiUrl)  //earlier it was console logging the thing return by this immidiately therfore we did not get
-    const data= await res.json()
-    console.log(data)
-}
-*/
+    // const res= await fetch(apiUrl)  //retuens a rreadable stream buffer which is not readable because its a buffer of bytes
+    // const data= await res.json()     //turns that to json
+    // console.log(data)
+    // const ghfollower= data.followers
 
-/*
-module.exports=async ()=>{
-    const res= await axios.get(apiUrl)
+    const res= await axios.get(apiUrl)   //if not awaited will return a promis but if awaited it will return the data passed to the resolve
     // console.log(res)
-    console.log(res.data)
-    console.log(res.data.followers)
-}
-*/
+    const ghfollower= res.data.followers
 
+    console.log(`
+Github Followers: ${ghfollower}
+    `)
+}
