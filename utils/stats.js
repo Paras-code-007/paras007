cffc4a30bf789900a0e55fa2aa147b796361e5ca
Paras Arora <parasarora@Parass-MacBook-Air.local>
2020-10-31 06:01:15 +0530

    custom error handling for axios using try catch await

--- utils/stats.js
+++ utils/stats.js
@@ -1,4 +1,5 @@
-// const fetch= require('node-fetch')
+const handleError= require('cli-handle-error')
+const fetch= require('node-fetch')
 const axios= require('axios')
 const apiUrl= 'https://api.github.com/users/Paras-code-007'
 
@@ -8,11 +9,30 @@ module.exports= async ()=>{
     // console.log(data)
     // const ghfollower= data.followers
 
-    const res= await axios.get(apiUrl)   //if not awaited will return a promis but if awaited it will return the data passed to the resolve
-    // console.log(res)
-    const ghfollower= res.data.followers
+    try {
+        const res= await axios.get(apiUrl)   //if not awaited will return a promis but if awaited it will return the data passed to the resolve
+        // console.log(res)
+        const ghfollower= res.data.followers
 
-    console.log(`
+        console.log(`
 Github Followers: ${ghfollower}
-    `)
+        `)
+    } catch (err) {
+        // console.log(err)
+
+        // console.log(err.name)
+        // console.log(err.message)
+        // console.log(err.stack)
+        //+Or
+        handleError('API CALL FAILED',err,true,false)
+    }
 }
+
+/*
+error from fetch in axios
+✖ UNHANDLED ERROR
+✖ ERROR → Error
+ℹ REASON → Request failed with status code 404
+
+error from catch was different
+*/
