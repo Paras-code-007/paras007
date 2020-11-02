e3d6774e49b8dc4a3d3c8fdc60a51e1f4d2f7c80
Paras-code-007 <paras.arora0025@gmail.com>
2020-11-03 00:36:42 +0530

    trying userinput with  readline

--- index.js
+++ index.js
@@ -10,17 +10,30 @@ const alert= require('clialerting')
 
 const args= process.argv.slice(2)
 
-;(async ()=>{
+const readline = require('readline');
 
-    cli.input.includes('help') && cli.showHelp(0)  
-    init(cli.flags)
-    cli.flags.bio && console.log(bio)
-    cli.flags.projects && console.log(projects)
-    cli.flags.social && console.log(socialHandles)        
-    cli.flags.fact && alert({type: 'info', msg: fact, name: 'fun fact'})
-    // await stats()  //wait for this because has async functions in it therfore need a synchtonous manner execution therofore had to wait here to 
-    cli.flags.repos && alert({type: 'info', msg: headrepo, name: sbrepo})
-    cli.flags.repos && (await repos())
-    debug(cli.flags.debug, cli.flags, cli.input)
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+  });
+
+;(async ()=>{
+    rl.question('What do you think of Node.js? ', (answer) => {
+        // TODO: Log the answer in a database
+        console.log(`Thank you for your valuable feedback: ${answer}`);
+      
+        rl.close();
+    });
+
+    // cli.input.includes('help') && cli.showHelp(0)  
+    // init(cli.flags)
+    // cli.flags.bio && console.log(bio)
+    // cli.flags.projects && console.log(projects)
+    // cli.flags.social && console.log(socialHandles)        
+    // cli.flags.fact && alert({type: 'info', msg: fact, name: 'fun fact'})
+    // // await stats()  //wait for this because has async functions in it therfore need a synchtonous manner execution therofore had to wait here to 
+    // cli.flags.repos && alert({type: 'info', msg: headrepo, name: sbrepo})
+    // cli.flags.repos && (await repos())
+    // debug(cli.flags.debug, cli.flags, cli.input)
 
 })()
